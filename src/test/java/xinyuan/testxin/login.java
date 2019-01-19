408e16399516da716d682fe95a8ab4f9efc4ffad
aa1234aa <1054003728@qq.com>
2019-01-19 10:14:42 +0800

    慕课自动化

--- src/test/java/xinyuan/testxin/login.java
+++ src/test/java/xinyuan/testxin/login.java
@@ -1,40 +1,62 @@
 package xinyuan.testxin;
 
 import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-public class login {
-    public WebDriver driver;
+@Listeners({ TestngListenerScreenShot.class })
+public class login extends baseDriver{
     public void InitDriver(){
-        System.setProperty("webdriver.chrome.driver","D:\\BaiduNetdiskDownload\\chromedriver.exe");
-        driver=new ChromeDriver();
         driver.get("https://www.imooc.com/");
         driver.manage().window().maximize();
-        try {
+       /* try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
         driver.findElement(By.id("js-signin-btn")).click();
-        try {
-            Thread.sleep(4000);
+        /*try {
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }*/
+
+
+    }
+
+    @Test
+    public void mytest() throws Exception{
+        HashMap<String,String> user=new HashMap<String, String>();
+        user.put("18210544883","anlili123");
+        Iterator us=user.entrySet().iterator();
+        while (us.hasNext()){
+            Map.Entry entry=(Map.Entry) us.next();
+            String username=entry.getKey().toString();
+            String password=entry.getValue().toString();
+            loginScript(username,password);
         }
     }
+
     //登陆脚本
     public void loginScript(String username,String password) throws Exception{
         this.InitDriver();
-        WebElement user=this.element(this.byStr("username"));
+       /* try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        WebElement user=this.element(this.byStr("username1"));
         user.isDisplayed();
         WebElement passwo=driver.findElement(By.name("password"));
         passwo.isDisplayed();
@@ -58,7 +80,7 @@ public class login {
             e.printStackTrace();
         }
         String userInfo=this.element(By.className("name")).getText();
-        if (userInfo.equals("平安24")) {
+        if (userInfo.equals("平安")) {
             System.out.println("登陆成功");
         } else {
             System.out.println("登陆失败");
@@ -86,19 +108,4 @@ public class login {
     }
 
 
-    public static void main(String[] args) throws Exception{
-        login login=new login();
-        HashMap<String,String> user=new HashMap<String, String>();
-        user.put("18210544883","anlili123");
-        user.put("aa","bb");
-        //如何读取
-        Iterator us=user.entrySet().iterator();
-        while (us.hasNext()){
-            Map.Entry entry=(Map.Entry) us.next();
-            String username=entry.getKey().toString();
-            String password=entry.getValue().toString();
-            login.loginScript(username,password);
-        }
-
-    }
 }
