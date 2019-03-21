8110f6b071e8681c2bcf33cc9457bf9c964652ad
John Doe <john@sample.com>
2019-03-21 14:30:22 +0800

    慕课自动化

---
+++ src/test/java/bitnei/page/HomePage.java
@@ -0,0 +1,25 @@
+package bitnei.page;
+
+import bitnei.base.DriverBase;
+import org.openqa.selenium.WebElement;
+import bitnei.testcase.getByLocator;
+
+/**
+ * anliping
+ * 预期判断
+ */
+public class HomePage extends BasePage{
+    public HomePage(DriverBase driver) {
+        super(driver);
+    }
+    //获取基本资料信息element
+    public WebElement getInfoElement(){
+        action(element(getByLocator.getLocator("rolename")));
+        return element(getByLocator.getLocator("basicinfo"));
+    }
+    //获取用户名
+    public WebElement getUserNmaeElement(){
+        return element(getByLocator.getLocator("name"));
+    }
+}
+
