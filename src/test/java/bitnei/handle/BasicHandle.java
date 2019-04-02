59a4795ebc4e259e21a712db83958adb51760a0c
John Doe <john@sample.com>
2019-04-02 20:33:18 +0800

    慕课自动化

---
+++ src/test/java/bitnei/handle/BasicHandle.java
@@ -0,0 +1,12 @@
+package bitnei.handle;
+
+import bitnei.base.DriverBase;
+import bitnei.page.BasicPage;
+
+/**
+ * 基础数据模块操作层
+ */
+public class BasicHandle {
+    public DriverBase driver;
+    public BasicPage bap;
+}
