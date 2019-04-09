fe5124b33c92d1543a478bb6e036a364da7a87b7
John Doe <john@sample.com>
2019-04-09 20:38:07 +0800

    慕课自动化

---
+++ src/test/java/bitnei/page/LookPage.java
@@ -0,0 +1,17 @@
+package bitnei.page;
+
+import bitnei.base.DriverBase;
+import org.openqa.selenium.WebElement;
+
+/**
+ * dfgdfg
+ */
+public class LookPage extends BasePage{
+    public LookPage(DriverBase driver) {
+        super(driver);
+    }
+    public void aa(){
+        System.out.println("11");
+    }
+}
+
